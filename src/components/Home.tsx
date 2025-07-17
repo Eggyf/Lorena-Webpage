433081e922db21d586493929bcb247ec1d1b60fc
Eggyf <abdelfregel1711@gmail.com>
2025-07-17 10:50:43 -0400

    begin

---
+++ src/components/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import AboutUs from './AboutUs';
+import WelcomePage from './WelcomePage';
+function Home() {
+  return (
+      <>
+          <WelcomePage />
+      <AboutUs/>
+      </>
+  )
+}
+
+export default Home
---
+++ src/components/WelcomePage.tsx
@@ -0,0 +1,24 @@
+import "../styles/welcomePage.css"
+
+function WelcomePage() {
+  return (
+    <>
+      <div className="navbar">
+        
+        <button className="sign-in-button">Sign In</button>
+      </div>
+      <div className="main">
+        <div className="main-buttons">
+          <button>
+            <img src="Iridiscencia W T-03.png" alt="hola" />
+          </button>
+          <button>
+            <img src="Iridiscencia W T-03.png" alt="hola" />
+          </button>
+            </div>
+      </div>
+    </>
+  )
+}
+
+export default WelcomePage
